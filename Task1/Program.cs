4acc8140815876006d168dd15ccc9d23f8328454
Pavel <pavel@b2profit.ru>
2023-11-17 18:49:21 +0300

    Добавил эталон первой задачи

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -20,3 +20,30 @@ for (int i = 0; i < matrix.GetLength(0); i++)
         }
         Console.WriteLine();
     }
+
+    // // Из эталона
+
+//   static Random random = new Random();
+
+  
+//     double[, ] matrix = new double[m, n];
+
+//     for (int i = 0; i < m; i++) 
+//     {
+//       for (int j = 0; j < n; j++) 
+//       {
+//         matrix[i, j] = random.Next(minLimitRandom, maxLimitRandom + 1) + random.NextDouble();
+//       }
+//     }
+
+//     for (int i = 0; i < matrix.GetLength(0); i++) 
+//     {
+//       for (int j = 0; j < matrix.GetLength(1); j++) 
+//       {
+//         Console.Write($"{matrix[i, j]:f2}\t");
+//       }
+//       Console.WriteLine();
+//     }
+
+   
+      
