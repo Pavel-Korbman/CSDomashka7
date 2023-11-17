70426ee7938bab195a1df0ec0e17537d4d8868f2
Pavel <pavel@b2profit.ru>
2023-11-17 22:46:19 +0300

    Добавил условие 2 задачи

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+int n = 4;
+int m = 5;
+int k = 3;
+int x = 2;
+int y = 2;
+
+int[,] matrix = new int[n, m];
+int sum = 1;
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+
+        matrix[i, j] = sum;
+        sum = sum + k;
+    }
+}
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        Console.Write($"{matrix[i, j]}\t");
+    }
+    Console.WriteLine();
+}
