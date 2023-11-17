12aeb46484b399986f4eaa8b36a1516b30b1cbed
Pavel <pavel@b2profit.ru>
2023-11-17 18:39:20 +0300

    Решил первую задачу

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int m = 3;
+int n = 4;
+int minLimitRandom = -10;
+int maxLimitRandom = 10;
+
+double[,] matrix = new double[m, n];
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = new Random().NextDouble() * (maxLimitRandom - minLimitRandom) + minLimitRandom;
+    }
+}
+
+ for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{Math.Round(matrix[i, j], 1)}\t");
+        }
+        Console.WriteLine();
+    }
